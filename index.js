b1f34571a94df0b6c5a1c11f9a858ca1b6c588bd
Conrad Pankoff <deoxxa@fknsrs.biz>
2013-01-08 19:24:07 +0900

    made it so there's multiple users and stuff
    yay i made this in the github editor i'm super cool

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 var net = require('net');
 
+var clients = [];
+
 var server = net.createServer(function(socket) {
+  var removed = false;
+  
+  clients.push(socket);
+  
+  function on_end() {
+    if (removed) {
+      return;
+    }
+
+    removed = true;
+
+    clients.splice(clients.indexOf(socket), 1);
+  }
+  socket.on("end", on_end);
+  socket.on("error", on_end);
+  
   socket.on('end', function() {
     console.log('sonarjs disconnected');
   });
   
   socket.write('beep boop...\r\n');
+  
+  socket.on("data", function(chunk) {
+    clients.forEach(function(client) {
+      if (client === socket) {
+        return;
+      }
+      
+      client.write(chunk);
+    });
+  });
   
   socket.pipe(socket);
 });
