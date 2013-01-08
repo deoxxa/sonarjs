da4c34d711cd83980b96693e2ab722906970c2ed
Naomi Kyoto <nakyoto@gmail.com>
2013-01-08 04:36:19 -0600

    support multiple dolphins

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 var net = require('net');
 
+var clients = [];
+
 var server = net.createServer(function(socket) {
-  socket.on('end', function() {
-    console.log('sonarjs disconnected');
+  
+  // open new socket
+  socket.write('beep boop...\r\n');
+  clients.push(socket);
+
+  // send to everyone
+  socket.on('data', function(chunk) {
+    clients.forEach(function(client) {
+
+      // don't echo to self
+      if (client === socket) {
+        return;
+      }
+
+      // send to each client
+      client.write(chunk);
+    });
   });
   
-  socket.write('beep boop...\r\n');
-  
-  socket.pipe(socket);
+  // connect stdin
+  process.stdin.resume();
+  process.stdin.on('data', function(data) {
+    socket.write(data);
+  });
+
+  // close socket
+  var onEnd = function() {
+    if (removed) {
+      return;
+    }
+    removed = true;
+    clients.splice(clients.indexOf(socket), 1);
+  };
+
+  socket.on('error', onEnd);
+  socket.on('end', onEnd);
+
 });
 
 server.listen(40404, function() {
